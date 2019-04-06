64879a65dfe1a159e7a01d27a576e851791eb67e
Dmitri Samotessov <dimasamo96@gmail.com>
2019-04-06 06:51:24 +0000

    Clicking on a movie on the results page now redirects to the specific movie show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,7 +19,10 @@ class MoviesController < ApplicationController
       @movies=@api.query["Search"]
       @movies.each do |movie|
         specific_movie = @api.query_specific(movie["Title"])
+        # binding.pry
         session[:movies] << (Movie.find_by(title: specific_movie["Title"]) or Movie.create(title: specific_movie["Title"], release_year: specific_movie["Year"], plot: specific_movie["Plot"], rating: specific_movie["imdbRating"], image_url: specific_movie["Poster"])).id
+        session[:movies].uniq!
+        session[:movies].compact!
       end
       redirect "/movies/results"
     else
