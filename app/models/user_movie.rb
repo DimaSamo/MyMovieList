4a2211578dc4ac035f1f6552a1a846c8b2a3d5b6
Dmitri Samotessov <dimasamo96@gmail.com>
2019-04-08 03:22:27 +0000

    Added icon for seen it in user_movies

--- app/models/user_movie.rb
+++ app/models/user_movie.rb
@@ -1,4 +1,11 @@
 class UserMovie < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
+  def seen?
+    if self.seen==true
+      "https://i.imgur.com/9ksHnF9.png"
+    else
+      "https://i.imgur.com/25r8sQK.png"
+    end
+  end
 end
