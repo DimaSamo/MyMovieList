b1182afa3b9636b94071df11b300b6aadd33f903
Dmitri Samotessov <dimasamo96@gmail.com>
2019-04-08 03:04:08 +0000

    Deleted unused routes in user_controller and unused erb pages

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,25 +1,10 @@
 class UserMoviesController < ApplicationController
 
-  # GET: /user_movies
-  get "/user_movies" do
-    erb :"/user_movies/index.html"
-  end
-
   #add movie to list
   post "/user_movies/new/:id" do
     @user_movie = UserMovie.new(user_id: current_user.id, movie_id: params[:id])
     @user_movie.save if !UserMovie.find_by(user_id: current_user.id, movie_id: params[:id])
     redirect "/"
-  end
-
-  # GET: /user_movies/new
-  get "/user_movies/new" do
-    erb :"/user_movies/new.html"
-  end
-
-  # POST: /user_movies
-  post "/user_movies" do
-    redirect "/user_movies"
   end
 
   # GET: /user_movies/5
